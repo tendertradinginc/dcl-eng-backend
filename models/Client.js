32466ecb6d2a8b278ab037d333ddf83424b973f9
husneara <husnearaa99@gmail.com>
2024-09-28 15:29:22 +0600

    has created the client schema

---
+++ models/Client.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const clientSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    image: {
+      type: String,
+      validate: {
+        validator: (value) =>
+          validator.isURL(value, {
+            protocols: ["http", "https"],
+            require_tld: true,
+            require_protocol: true,
+          }),
+
+        message: "Invalid image URL",
+      },
+      required: true,
+    },
+
+    featuredStatus: {
+      type: boolean,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Client = mongoose.model("Client", clientSchema);
+module.exports = Client;
