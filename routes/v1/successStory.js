8b3177055a6129d0f3a1d65b25af9bc7e4c3148a
husneara <husnearaa99@gmail.com>
2024-09-30 13:48:54 +0600

    has created the success story route

---
+++ routes/v1/successStory.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const successStoryController = require("../../controllers/successStory.controller");
+
+router.route("/create").post(successStoryController.createSuccessStory);
+
+router.route("/").get(successStoryController.getAllSuccessStory);
+
+router
+    .route("/:id")
+    .delete(successStoryController.deleteSuccessStory)
+    .put(successStoryController.updateSuccessStory);
+
+module.exports = router;
