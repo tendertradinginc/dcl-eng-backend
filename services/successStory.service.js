67377fe433b6329f5a590a1dd65e0d3ae6d4f29a
husneara <husnearaa99@gmail.com>
2024-10-04 18:50:20 +0600

    has added search function in the success story services for dashboard

--- services/successStory.service.js
+++ services/successStory.service.js
@@ -5,14 +5,43 @@ exports.createSuccessStoryinDb = async (details) => {
     return result;
 };
 
-exports.getAllSuccessStoryinDb = async (page, limit) => {
-    const result = await SuccessStory.find()
+// exports.getAllSuccessStoryinDb = async (page, limit) => {
+//     const result = await SuccessStory.find()
+//         .sort({ name: "asc" })
+//         .limit(parseInt(limit))
+//         .skip((parseInt(page) - 1) * parseInt(limit));
+//     const total = await SuccessStory.countDocuments();
+//     return { result, total };
+// };
+
+
+
+exports.getAllSuccessStoryinDb = async (page, limit, search = "") => {
+
+    const searchTerm = typeof search === "undefined" || search === null ? "" : search;
+    const regexSearch = new RegExp(searchTerm, "i");
+
+    let query = {};
+    if (searchTerm) {
+        query.$or = [
+            { name: { $regex: regexSearch } },
+            { shortDescription: { $regex: regexSearch } },
+            { fullDescription: { $regex: regexSearch } }
+        ];
+    }
+
+    const result = await SuccessStory.find(query)
         .sort({ name: "asc" })
         .limit(parseInt(limit))
         .skip((parseInt(page) - 1) * parseInt(limit));
-    const total = await SuccessStory.countDocuments();
+
+    const total = await SuccessStory.countDocuments(query);
+
     return { result, total };
 };
+
+
+
 
 exports.deleteSuccessStoryinDb = async (id) => {
     const result = await SuccessStory.deleteOne({ _id: id });
