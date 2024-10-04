f88185d3a800f7b2e73180d064b3de13bb2405f5
sakil-sagor <sakilsagor76543@gmail.com>
2024-10-05 00:10:30 +0600

    crate banner page schema and app function

---
+++ models/Banner.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const BannerSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      default: "",
+    },
+    colorWord: {
+      type: String,
+      default: "",
+    },
+    lastTitle: {
+      type: String,
+      default: "",
+    },
+    subtitle: {
+      type: String,
+      default: "",
+    },
+    backgroundImage: {
+      type: String,
+      validate: {
+        validator: (value) =>
+          validator.isURL(value, {
+            protocols: ["http", "https"],
+            require_tld: true,
+            require_protocol: true,
+          }),
+
+        message: "Invalid image URL",
+      },
+      required: true,
+    },
+    buttonText: {
+      type: String,
+      default: "/services",
+    },
+    location: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Banner = mongoose.model("Banner", BannerSchema);
+module.exports = Banner;
