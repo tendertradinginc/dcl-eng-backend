dc376192cef9738b552a25f467aef7913240f2e6
sakil-sagor <sakilsagor76543@gmail.com>
2024-10-05 14:14:22 +0600

    create bammer page dashboard all route

--- routes/v1/homepage.route.js
+++ routes/v1/homepage.route.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const homepageController = require("../../controllers/homepage.controller");
 // user create
 router.route("/summary").get(homepageController.getAllSummary);
+router.route("/banner").get(homepageController.getAllBanner);
 
 module.exports = router;
